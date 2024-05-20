14d0f75c4936f2949337005caa88c98e3fd5b912
Mateus <mateusmpi@gmail.com>
2024-05-20 19:31:14 -0300

    fix: send message via input

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 let messages = [];
 let userName;
 
-function scrollToBottom() {
-    let container = document.querySelector(".container");
-    container.scrollTop = container.scrollHeight - window.innerHeight;
-}
+let typedMessage = {
+	from: "",
+	to: "Todos",
+	text: "",
+	type: "message" // ou "private_message"
+};
+
 
 function showMessages() {
     let messageList = document.querySelector(".container ul");
@@ -12,13 +15,27 @@ function showMessages() {
     
     for(let i = 0; i < messages.length; i++) {
         let message = messages[i];
-        messageList.innerHTML += `
-            <li>
+        // console.log(messages[i]);
+        if (message.type === 'status'){
+            messageList.innerHTML += `
+            <li class="messageItem">
                 <p class="enterLeaveChat"><span>(${message.time}) </span> <em>${message.from}</em> ${message.text}</p>
             </li>
         `;
-    }    
-    scrollToBottom();
+        } else if (message.type === 'message') {
+            messageList.innerHTML += `
+            <li class="messageItem">
+                <p><span>(${message.time}) </span> <em>${message.from}</em> para <em>${message.to}</em>: ${message.text}</p>
+            </li>
+        `;
+        }
+    }
+
+    let lastMessageItem = document.querySelector(".messageItem:last-child");
+    // console.log(lastMessageItem);
+    if (lastMessageItem) {
+        lastMessageItem.scrollIntoView();
+    }
 }
 
 function responseData(serverData) {
@@ -27,10 +44,10 @@ function responseData(serverData) {
     showMessages();
 }
 
-setInterval(statusOk, 5000);
+// setInterval(statusUser, 5000);
 
 function statusOk() {
-    console.log("O usuário continua on");
+    // console.log("O usuário continua on");
 }
 
 function statusUser() {
@@ -45,8 +62,13 @@ function getMessages() {
     promise.catch(showError);
 }
 
-function showError() {
-    alert("Ocorreu um erro. Tente mais tarde.")
+function showError(erro) {
+
+    if (erro.response && erro.response.status === 400) {
+        alert("Verifique se você preencheu corretamente o nome do usuário!");
+    } else {
+        alert("Ocorreu um erro. Tente novamente mais tarde.");
+    }
 }
 
 function receiveResponse(response) {
@@ -59,14 +81,66 @@ function receiveResponse(response) {
     getMessages();
 }
 
-function askName(){
-        userName = prompt("Digite seu nome:");
+function namesSent(names) {
+    let userExists = false; 
+    // console.log(names);
+
+    for (let i = 0; i < names.data.length; i++){
+        if (userName === names.data[i].name) {
+            userExists = true;
+            break;
+        }
+    } if (userExists) {
+        alert(`${userName} já existe! Adicione outro nome.`);
+        askName();
+    } else {
         const promise = axios.post('https://mock-api.driven.com.br/api/v6/uol/participants/9b93bd8b-fdc8-47f6-ab3c-dc122a80b154', {name: userName});
         promise.then(receiveResponse);
-        promise.catch(showError);        
+        promise.catch(showError);   
+    }
 }
+
+function checkName() {
+    const promise = axios.get("https://mock-api.driven.com.br/api/v6/uol/participants/9b93bd8b-fdc8-47f6-ab3c-dc122a80b154");
+    promise.then(namesSent);
+    promise.catch(showError);
+}
+
+function askName(){
+    userName = prompt("Digite seu nome:");
+    typedMessage.from = userName;
+    checkName();     
+}
+
+function messageWasSent() {
+    console.log("Mensagem foi!")
+}
+
+function sendMessage() {
+    const input = document.getElementById("myInput");
+    typedMessage.text = input.value.trim();
+
+    if (typedMessage.text) {
+        console.log(typedMessage.text);
+        axios.post("https://mock-api.driven.com.br/api/v6/uol/messages/9b93bd8b-fdc8-47f6-ab3c-dc122a80b154", typedMessage)
+            .then(messageWasSent)
+            .catch(showError);
+        input.value = ""; // Limpar o input após enviar a mensagem
+    } else {
+        alert("Digite uma mensagem antes de enviar!");
+    }
+}
+
+document.getElementById("submitButton").addEventListener("click", sendMessage);
+
+document.getElementById("myInput").addEventListener("keyup", function(event) {
+    if (event.key === "Enter") {
+        sendMessage();
+    }
+});
 
 getMessages();
 askName();
-
+setInterval(getMessages, 3000);
+setInterval(statusUser, 5000);
 
