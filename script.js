e9ea7dcf21e7fbf00288ee807aedda1b5936d6ad
Mateus <mateusmpi@gmail.com>
2024-05-21 22:16:20 -0300

    fix: display only public or personal message

--- script.js
+++ script.js
@@ -55,6 +55,7 @@ function responseData(serverData) {
     })
     messages = filterMessages;
     
+    console.log(messages);
     showMessages();
 }
 
@@ -217,6 +218,8 @@ function selectElement(event) {
                     <p>Enviando para ${typedMessage.to} (${visibilityText})</p>
                 `;
             } else if (typedMessage.to !== "Todos" && visibilityElement.innerHTML === "Reservadamente") {
+                console.log(visibilityElement.innerHTML)
+                console.log(typedMessage.to)
                 const visibilityText = visibilityElement.textContent;
                 typedMessage.type = "private_message";
                 typedMessage.to= `${typedMessage.to}`;
@@ -225,6 +228,8 @@ function selectElement(event) {
                     <p>Enviando para ${typedMessage.to} (${visibilityText})</p>
                 `;
             } else if (typedMessage.to !== "Todos" && visibilityElement.innerHTML === "Público") {
+                console.log(visibilityElement.innerHTML)
+                console.log(typedMessage.to)
                 const visibilityText = visibilityElement.textContent;
                 typedMessage.type = "message";
                 typedMessage.to= `${typedMessage.to}`;
@@ -234,6 +239,8 @@ function selectElement(event) {
                 `;
             } else {
                 typedMessage.type = "message";
+                console.log(visibilityElement.innerHTML)
+                console.log(typedMessage.to)
                 const secondLiIconCheckMenu = document.querySelector(".visibility li:nth-child(2) .iconCheckMenu");
                 const firstLiIconCheckMenu = document.querySelector(".visibility li:nth-child(1) .iconCheckMenu");
 
